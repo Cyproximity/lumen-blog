f1d14dd00870eff03cefc0565cc6d834819b4d9e
Gerald Agustin <geraldagustin09@gmail.com>
2016-07-14 22:45:21 +0800

    frontend files

---
+++ frontend/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path"),
+			webpack = require("webpack"),
+			libPath = path.join(__dirname, 'src');
+
+//plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/app.js',
+	output: {
+		path: './build',
+		filename: 'app.bundle.js'
+	}, 
+	module: {
+		loaders: [
+			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+			{ test: /\.jade$/, loader: "jade" },
+			{ test: /\.scss$/, loaders: ["style", "css", "sass"] }
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(libPath, 'index.html'),
+			filename: 'index.html',
+		}),
+	]
+}
+
+// console.log(path, webpack);
