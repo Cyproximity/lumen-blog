a4e06f8ff637e1146a8d4331088ecde6acb5444a
Gerald Agustin <gerald.agustin09@gmail.com>
2016-08-28 18:31:59 +0800

    react

--- frontend/src/javascript/modules.js
+++ frontend/src/javascript/modules.js
@@ -8,13 +8,13 @@ import index from './components/index';
 const coreApplication = document.getElementById('core');
 
 class Core extends React.Component {
-    constructor(props) {
-        super(props);
-        this.displayName = 'Core';
-    }
-    render() {
-        return <div>{this.displayName}</div>;
-    }
+  constructor(props) {
+    super(props);
+    this.displayName = 'Core';
+  }
+  render() {
+    return <div>{this.displayName}</div>;
+  }
 }
 
 ReactDOM.render(<Core />, coreApplication);
