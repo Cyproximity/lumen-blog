63b8f81c1de4a33245a9dca15c4fef546508df7a
Gerald Agustin <gerald.agustin09@gmail.com>
2016-08-28 16:30:46 +0800

    front-end final webpack build

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,6 +1,3 @@
-//3rd party libraries
-import './vendor';
-
 //all resources
 import './stylesheets/styles';
 import './javascript/modules';
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,65 +1,78 @@
+'use strict';
+
+var DEV_ENV = {
+  host: '127.0.0.1',
+  port: 8080,
+  target: 'http://127.0.0.1:8080'
+}
+
 // node environment
-const env = process.env.npm_lifecycle_event;
+var env = process.env.npm_lifecycle_event,
+    path = require('path'),
+    webpack = require('webpack'),
+    merge = require('webpack-merge'),
+    autoprefixer = require('autoprefixer'),
+    precss = require('precss');
 
-const path = require('path'),
-      webpack = require('webpack'),
-      merge   = require('webpack-merge'),
-      libPath = path.join(__dirname),
-      srcPath = path.join(__dirname, 'src'),
-      pubPath = path.join(__dirname, 'build'),
-      bowPath = path.join(__dirname, 'bower_components'),
-      nodPath = path.join(__dirname, 'node_modules');
-
+var PATH_DIR = {
+  lib: path.join(__dirname),
+  src: path.join(__dirname, 'src'),
+  pub: path.join(__dirname, 'build'),
+  bower_path: path.join(__dirname, 'bower_components'),
+  node_Path: path.join(__dirname, 'node_modules')
+}
 
 //plugins
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-      CleanWebpackPlugin = require('clean-webpack-plugin'),
-      ExtractTextPlugin = require('extract-text-webpack-plugin'),
-      CopyWebpackPlugin = require('copy-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin"),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    CleanWebpackPlugin = require('clean-webpack-plugin'),
+    CopyWebpackPlugin = require('copy-webpack-plugin');
 
-//3rd parties
-const precss = require('precss'),
-      autoprefixer = require('autoprefixer'),
-      postcss = function () {
-        return { default: [precss, autoprefixer]};
-      };
 
-const config = {
-  entry: './src/app.js',
+//default config
+var config = {
+  target: 'web',
+  context: PATH_DIR.src,
+  entry: {
+    vendors: path.resolve( PATH_DIR.src, 'vendors.js'),
+    app: path.resolve( PATH_DIR.src, 'app.js' )
+  },
   output: {
-    path: pubPath,
+    path: PATH_DIR.pub,
     publicPath: '/',
-    filename: 'app.[id]-[hash].js'
+    filename: 'js/bundle.[name].[id]-[hash].js'
   },
   resolve: {
-    extensions: ['', '.js'],
-    modulesDirectories: [ 'bower_components', 'node_modules', srcPath ]
+    root: PATH_DIR.lib,
+    extensions: ['', '.js', '.jsx'],
+    modulesDirectories: ['node_modules', 'bower_components', PATH_DIR.src]
   },
   module: {
     loaders: [
-      { test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: { presets: ['es2015', 'stage-2', 'react'] }
-      },
-      {
-        test: /\.jade$/,
-        loader: 'raw!html!jade!pug'
-      },
       {
         test: /\.css$/,
-        loader: 'style!css'
+        loader: ExtractTextPlugin.extract('style!css')
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'stage-2', 'react']
+        }
       },
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract({
-          fallbackLoader: 'style-loader',
-          loader: ['style','css', 'sass', 'postcss']
-        })
+        loader: ExtractTextPlugin.extract('style', 'css!sass!postcss')
+      },
+      {
+        test: /\.jade$/,
+        loader: 'pug'
       },
       {
         test: /.*\.(gif|png|jpe?g|svg)$/i,
         loaders: [
+          'url-loader',
           'file?hash=sha512&digest=hex&name=[hash].[ext]',
           'image-webpack'
         ]
@@ -67,67 +80,98 @@ const config = {
     ]
   },
   sassLoader: {
-    includePaths: [ bowPath, nodPath, path.resolve(srcPath, 'stylesheets') ]
+    includePaths: [path.resolve(PATH_DIR.src)]
   },
-  plugins: [
-    new CleanWebpackPlugin(['build'], {
-      root: libPath,
-      verbose: true,
-      dry: false,
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.ResolverPlugin(
-      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin(".bower.json", ["main"])
-    ),
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: path.join(srcPath, 'index.html'),
-      filename: 'index.html',
-    }),
-    new ExtractTextPlugin({
-      filename: 'styles.[id]-[name].css',
-      allChunks: false
-    }),
-    new CopyWebpackPlugin([
-      { from: path.join(srcPath, 'images'), to: path.join(pubPath, 'images') }
-    ]),
-
-  ]
-};
-
-const devConfig = {
-  hot: true,
-  contentBase: pubPath,
-  outputPath: pubPath,
-  historyApiFallback: true,
-  proxy: {
-    '/api/*': {
-      target: 'http://localhost:8080',
-      changeOrigin: true,
-      secure: false
+  postcss: function () {
+    return {
+      plugins: [autoprefixer, precss]
     }
   },
+  imageWebpackLoader: {
+    pngquant:{
+      quality: "65-90",
+      speed: 4
+    },
+    svgo:{
+      plugins: [
+        { removeViewBox: false },
+        { removeEmptyAttrs: false }
+      ]
+    }
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new CleanWebpackPlugin(['build'], {
+      root: PATH_DIR.lib,
+      verbose: true
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      filename: 'index.html',
+      template: PATH_DIR.src + '/index.html',
+      favicon: PATH_DIR.src + '/images/logo-new.png'
+    }),
+    new ExtractTextPlugin('css/bundle.[name].[id]-[hash].css')
+  ]
 }
 
-// TODO: image-loader or image min
-
-if(env === 'start') {
-  module.exports = merge(config, {
-    plugins: [
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false
+function Devserver(host, port, target) {
+  return {
+    devTool: 'eval',
+    devServer: {
+      hot: true,
+      inline: true,
+      progress: true,
+      historyApiFallback: true,
+      host: host,
+      port: port,
+      contentBase: PATH_DIR.pub,
+      outputPath: PATH_DIR.pub,
+      stats: {
+        colors: true
+      },
+      proxy: {
+        '/api/*': {
+          target: target,
+          changeOrigin: true,
+          secure: false
         }
-      })
+      },
+      watchOptions: {
+        poll: false
+      }
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new CopyWebpackPlugin([
+        { from: path.resolve(PATH_DIR.src, 'images'), to: '/images' }
+      ])
+    ]
+  }
+}
+
+// Servers
+if(env === 'start'){
+  module.exports = merge(config, {});
+}
+
+if(env === 'dev'){
+  module.exports = merge(config, Devserver(DEV_ENV.host, DEV_ENV.port, DEV_ENV.port));
+}
+
+if(env === 'build'){
+  module.exports = merge(config, {
+    plugins: [
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        mangle: false,
+        sourceMap: false,
+        compress: { warnings: false }
+      }),
+      new CopyWebpackPlugin([
+        { from: path.resolve(PATH_DIR.src, 'images'), to:  path.resolve(PATH_DIR.pub, 'images') }
+      ])
     ]
   });
 }
 
-if(env === 'dev') {
-  module.exports = merge(config, {
-    devServer: devConfig,
-    plugins: [
-      new webpack.HotModuleReplacementPlugin()
-    ]
-  });
-}
